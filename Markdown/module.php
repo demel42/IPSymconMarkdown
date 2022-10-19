9f164e044ab684338c44fa1e220c0c606bd86ab7
Ch. Damsky <demel42@web.de>
2022-10-19 17:18:46 +0200

    v1.0

--- Markdown/module.php
+++ Markdown/module.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 require_once __DIR__ . '/../libs/common.php';
 require_once __DIR__ . '/../libs/local.php';
 
-require_once __DIR__ . '/../parsedown/Parsedown.php';
+require_once __DIR__ . '/../libs/parsedown/Parsedown.php';
 
 class Markdown extends IPSModule
 {
